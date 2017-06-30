25fb218fb12113c885ca26ed6980a33576f9445e
angus.chen <angus.chen@anguschende-MacBook-Pro.local>
2017-06-30 14:11:06 +0800

    feat: collection module

---
+++ api/controllers/collection.js
@@ -0,0 +1,94 @@
+const Hash = require("string-hash")
+const List = require("collections/list")
+const Base64 = require("js-base64")
+const fs = require("fs")
+
+module.exports = {
+	Collection,
+}
+
+// collection to save K-V
+function Collection(){
+  // hashIdx: hash value, value: LinkedList with DataItem
+  this.hashIdx = [];
+}
+
+// open a local file to read data
+Collection.prototype.connect = function(){
+	// 暫時先不寫，最後再補
+}
+
+Collection.prototype.findOne = function(key){
+	let idx = Hash.hash(key),
+			list = this.hashIdx[idx];
+
+	if(!list || list.length === 0){
+		// not found
+		return false;
+	}else{
+		// include same hash idx, one hash idx
+		// same hash index, compare origin key, return first match same keys
+		let target = list.filter((doc) => doc.key === key).toArray();
+		if(target.length > 1) console.log(`find result has multi same keys with docs: ${target}`);
+		return (target.length === 0) ? false : target;
+	}
+}
+
+Collection.prototype.insertOne = function(key, value){
+	let idx = Hash.hash(key),
+			list = this.hashIdx[idx],
+			doc = new Document(key, value);
+
+	if(!list){  list = new List();  }
+	// append old list
+	list.push(doc);
+	return idx;  // return hash index
+}
+
+Collection.prototype.findOneAndUpdate = (key, value) => {
+	let idx = Hash.hash(key),
+			doc = new Document(key, value);
+
+	if(!this.findOne(key)){
+		return this.insert(key, value);
+	}else{
+		let list = this.hashIdx[idx];
+		return list.some((item) => {
+			if(item.key === doc.key){
+				item.value = doc.value;
+				return true;
+			}
+			return false;
+		})
+	}
+}
+
+Collection.prototype.findOneAndDelete = (key) => {
+	let idx = Hash.hash(key),
+			doc = new Document(key, value);
+
+	if(!this.findOne(key)){
+		return new Error(`no document found with key: ${key}`);
+	}else{
+		let list = this.hashIdx[idx],
+				target = list.filter((doc) => doc.key === key).toArray();
+		
+		let oldValue;
+		list.some((item) => {
+			if(item.key === doc.key){
+				oldValue = item.value;
+				item.value = null;
+				return true;
+			}
+			return false;
+		})
+		return (!oldValue) ? false : oldValue;
+	}
+}
+
+// for every k-v store
+// 相同雜湊值時要怎麼判別需求哪個？ =>  origin base64 相同
+function Document(key, value){
+	this.key = key;      // url-safe base64
+	this.value = value;  // base64
+}
--- api/controllers/kv.js
+++ api/controllers/kv.js
@@ -1,6 +1,21 @@
+const URLSafeBase64 = require("urlsafe-base64")
 let now = '2017-06-29T07:36:17.653Z'
 
 function getKEY(req, res) {
+  // check
+    // out-spec check
+      // abnormal input
+
+    // in-spect check
+      // KvKey base64 format
+      // 
+    
+  // process
+    // find
+      // hashmap search
+    // not-found
+
+  // response
   res.json(404, {
     VALUE: "hello world",
     TS: now,
@@ -8,12 +23,17 @@ function getKEY(req, res) {
 }
 
 function deleteKEY(req, res) {
+  // check
+  // findOneAndDelete
+  // response
   res.json(200, {
     TS: now,
   })
 }
 
 function postKEY(req, res) {
+  // check
+  // findOneAndUpdata or insertOne
   res.json(200, {
     TS: now,
   })
@@ -24,3 +44,5 @@ module.exports = {
   deleteKEY,
   postKEY,
 }
+
+
